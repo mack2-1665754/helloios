d66dbd2475d5431b0a9bf8ee4f7d9a7b5086a7db
mackenziehutchison <mack2@uw.edu>
2021-04-06 21:33:37 -0700

    finished assignment 1

---
+++ Shared/helloiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloiosApp.swift
+//  Shared
+//
+//  Created by stlp on 4/5/21.
+//
+
+import SwiftUI
+
+@main
+struct helloiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
